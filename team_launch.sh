51825936f3af484343044117723f7297964765ca
Leonid E <evlan@evlan.x>
2016-05-27 12:41:14 +0400

    launch scripts are corrected in order to work correctly with big num parameters

--- team_launch.sh
+++ team_launch.sh
@@ -8,14 +8,15 @@ fi
 if [ $num -gt 0 ]
     then
         let flag=1
+        let triesCnt=0
         while [ $flag -eq 1 ]
         do
-            echo "num="$num
             wget <link_to_team_ezquake_log> -O - | tac | grep begun -m $num -A 1 -B 10000 | tac | python getstats.py --net-log
-            if [[ $? -eq 0 || $? -eq 2 || $num -gt 100 ]]
+            if [[ $? -eq 0 || $? -eq 2 || $triesCnt -gt 100 ]]
                 then
                     let flag=0
             fi
             let num=$num+1
+            let triesCnt=$triesCnt+1
         done
 fi
