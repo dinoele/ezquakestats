87c680dcba8f283ef19ea6b8d71f497045e81753
Leonid E <evlan@evlan.x>
2016-12-09 10:35:40 +0400

    generate_tournament_table.py: refactoring

--- generate_tournament_table.py
+++ generate_tournament_table.py
@@ -1,5 +1,23 @@
-
+#!/usr/bin/python
 import subprocess
+
+groupALogs = [
+              "PL_[dad2]_2016-12-07_10_43_20.html",
+              "PL_[blizz]_2016-12-06_11_35_38.html",
+              "PL_[baldm6]_2016-12-06_13_09_20.html",
+              "PL_[utressor]_2016-12-07_12_50_30.html",
+              "PL_[warfare]_2016-12-08_12_20_48.html",
+              "PL_[travelert6]_2016-12-08_13_45_53.html",
+             ]
+
+groupBLogs = [
+              "FD_[warfare]_2016-12-06_13_11_29.html",
+              "FD_[travelert6]_2016-12-06_11_35_15.html",
+              "FD_[dad2]_2016-12-07_10_36_10.html",
+              "FD_[skull]_2016-12-07_12_53_46.html",
+              "FD_[blizz]_2016-12-08_11_31_18.html",
+              "FD_[aerowalk]_2016-12-08_13_24_12.html",
+             ]
 
 T_HEADER = \
 "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n" \
@@ -30,11 +48,13 @@ T_HEADER = \
 
 
 
-T_MAIN = \
+T_MAIN_HEADER = \
 "<body>\n" \
-"<div id=\"main\">\n" \
+"<div id=\"main\">\n"
+
+T_GROUP_A = \
 "	<div>\n" \
-"		<h1>Group A</h1>\n" \
+"		<h1>Group APOINT_ALGORITHM_PLACE</h1>\n" \
 "		<table cellspacing=\"1\" class=\"tablesorter\">\n" \
 "		<thead>\n" \
 "				<tr>\n" \
@@ -62,9 +82,12 @@ T_MAIN = \
 "GROUP_A_LOGS" \
 "			</tbody>\n" \
 "		</table>\n" \
-"	</div>\n" \
+"	</div>\n"
+
+
+T_GROUP_B = \
 "	<div>\n" \
-"		<h1>Group B</h1>\n" \
+"		<h1>Group BPOINT_ALGORITHM_PLACE</h1>\n" \
 "		<table cellspacing=\"1\" class=\"tablesorter\">\n" \
 "		<thead>\n" \
 "				<tr>\n" \
@@ -91,7 +114,9 @@ T_MAIN = \
 "GROUP_B_LOGS" \
 "			</tbody>\n" \
 "		</table>\n" \
-"	</div>\n" \
+"	</div>\n"
+
+T_MAIN_FOOTER = \
 "</div>\n" \
 "</body>\n" \
 "</html>"
@@ -119,9 +144,6 @@ allMaps = ["warfare", "travelert6", "dad2", "skull", "blizz", "aerowalk", "baldm
 groupA = ["zrkn", "Onanim", "SHAROK", "EEE", "random"]
 groupB = ["ss", "AREXP", "girgelezobeton", "ASHOT_HEADSHOT", "twinhooker", "bw"]
 
-groupALogs = ["PL_[dad2]_2016-12-07_10_43_20.html","PL_[blizz]_2016-12-06_11_35_38.html","PL_[baldm6]_2016-12-06_13_09_20.html","PL_[utressor]_2016-12-07_12_50_30.html"]
-groupBLogs = ["FD_[warfare]_2016-12-06_13_11_29.html","FD_[travelert6]_2016-12-06_11_35_15.html","FD_[dad2]_2016-12-07_10_36_10.html","FD_[skull]_2016-12-07_12_53_46.html"]
-
 def generatePlayerTR(plName, pointsDict):
     res = PLAYER_TR
     res = res.replace("PLAYER_NAME_PLACE", plName)
@@ -137,14 +159,17 @@ def generatePlayerTR(plName, pointsDict):
     res = res.replace("PLAYER_TOTAL_POINTS_PLACE", str(totalPoints))
     
     return res
+
+def generateGroupDiv(yy, groupName, pointsAlg = 1, isDescription = False):
+    group     = groupA if groupName == "A" else groupB
+    groupLogs = groupALogs if groupName == "A" else groupBLogs
     
-def A():
     pointsD = {}
-    for nn in groupA:
+    for nn in group:
         pointsD[nn] = {}    
     
     playedMaps = []
-    for log in groupALogs:
+    for log in groupLogs:
         mapName = log.split("_")[1].replace("[","").replace("]","")
         playedMaps.append([mapName, log])
         
@@ -169,7 +194,18 @@ def A():
             pointsD[name][mapName] = pnt
             
             i -= 1
-            pnt += 1 if i != 0 else 2
+            
+            if pointsAlg == 1:
+                pnt += 1 if i != 0 else 2
+            elif pointsAlg == 2:
+                if i == 0:
+                    pnt += 3
+                elif i == 1:
+                    pnt += 2
+                else:
+                    pnt += 1
+            elif pointsAlg == 3:
+                pnt += 1
     
     
     tt = ""    
@@ -191,69 +227,22 @@ def A():
     
     ff = MATCH_LOGS_TR.replace("PPPPPP", uu)
     
-    yy = T_MAIN.replace("GROUP_A_MAIN", tt)
-    yy = yy.replace("GROUP_A_LOGS", ff)
+    yy = yy.replace("GROUP_%s_MAIN" % (groupName), tt)
+    yy = yy.replace("GROUP_%s_LOGS" % (groupName), ff)
+    
+    pointsDescr = ""
+    if pointsAlg == 1:
+        pointsDescr = "  (1->2->3->4->6)" if groupName == "A" else "  (1->2->3->4->5->7)"
+    elif pointsAlg == 2:
+        pointsDescr = "  (1->2->3->5->8)" if groupName == "A" else "  (1->2->3->4->6->9)"
+    elif pointsAlg == 3:
+        pointsDescr = "  (1->2->3->4->5)" if groupName == "A" else "  (1->2->3->4->5->6)"
+
+    yy = yy.replace("POINT_ALGORITHM_PLACE", pointsDescr if isDescription else "")
     
     return yy
 
-def B(yy):
-    pointsD = {}
-    for nn in groupB:
-        pointsD[nn] = {}    
-    
-    playedMaps = []
-    for log in groupBLogs:
-        mapName = log.split("_")[1].replace("[","").replace("]","")
-        playedMaps.append([mapName, log])
-        
-        logHeadStr = subprocess.check_output(["head", "%s" % ("../" + log)])
-        if "GAME_PLAYERS" in logHeadStr:
-            playsStr = logHeadStr.split("GAME_PLAYERS")[1].split("-->")[0]
-                
-        plays = []
-        if playsStr != "":
-            playsStr = playsStr.replace("\n","")
-            plays = playsStr.split(" ")
-        
-        plCount = len(plays)
-        i = len(plays) - 1
-        pnt = 1
-        while i >= 0:
-            name = plays[i].split("(")[0]
-            
-            if not pointsD.has_key(name):
-                pointsD[name] = {}
-            
-            pointsD[name][mapName] = pnt
-            
-            i -= 1
-            pnt += 1 if i != 0 else 2
-    
-    
-    tt = ""    
-    for k in pointsD.keys():
-        tt += generatePlayerTR(k, pointsD[k])
-    
-    uu = ""
-    for m in allMaps:
-            
-        logN = ""
-        for xx in playedMaps:
-            if xx[0] == m:
-                logN = xx[1]
-        
-        if logN != "":
-            uu += MATCH_LOGS_FILLED_TD.replace("LOG_NAME", logN)
-        else:
-            uu += MATCH_LOGS_EMPTY_TD
-    
-    ff = MATCH_LOGS_TR.replace("PPPPPP", uu)
-    
-    yy = yy.replace("GROUP_B_MAIN", tt)
-    yy = yy.replace("GROUP_B_LOGS", ff)
-    
-    return yy
+# print T_HEADER + T_MAIN_HEADER + generateGroupDiv(T_GROUP_A, "A") + generateGroupDiv(T_GROUP_B, "B") + T_MAIN_FOOTER
 
-hh = A()
-
-print T_HEADER + B(hh)
+print T_HEADER + T_MAIN_HEADER + generateGroupDiv(T_GROUP_A, "A", 1, True) + generateGroupDiv(T_GROUP_A, "A", 2, True) + generateGroupDiv(T_GROUP_A, "A", 3, True) + \
+                                 generateGroupDiv(T_GROUP_B, "B", 1, True) + generateGroupDiv(T_GROUP_B, "B", 2, True) + generateGroupDiv(T_GROUP_B, "B", 3, True) + T_MAIN_FOOTER
