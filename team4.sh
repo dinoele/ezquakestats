063486b6da7148b66811ec2df3f40d909cea6698
Leonid E <evlan@evlan.x>
2016-12-16 15:16:13 +0400

    team*.sh files are added

---
+++ team4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+let num=1
+if [ $# -eq 1 ]
+  then
+    num=$1
+fi
+
+if [ $num -gt 0 ]
+    then
+        let flag=1
+        let triesCnt=0
+        while [ $flag -eq 1 ]
+        do
+            echo "num="$num
+            cat /home/random/qwserver/quaketeam4.log -O - | tac | grep begun -m $num -A 5 -B 10000 | tac | python getstats_deathmatch.py --league "Number 4"
+            if [[ $? -eq 0 || $? -eq 2 || $triesCnt -gt 100 ]]
+                then
+                    let flag=0
+            fi
+            let num=$num+1
+            let triesCnt=$triesCnt+1
+        done
+fi
