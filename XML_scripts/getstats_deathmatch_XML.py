82fbd3b3c4213a83074f88b2f87badc52efc13cc
dinoele <evlan@yandex.ru>
2020-05-26 20:28:21 +0300

    getstats_deathmatch_XML.py: lifetime charts are sorted by frags

--- XML_scripts/getstats_deathmatch_XML.py
+++ XML_scripts/getstats_deathmatch_XML.py
@@ -2162,7 +2162,7 @@ def writeHtmlWithScripts(f, sortedPlayers, resStr):
     
     # highcharts players lifetime -->
     playersLifetimeDivStrs = ""
-    for pl in allplayers:
+    for pl in allplayersByFrags:
         playersLifetimeDivStrs += ezstatslib.HTML_SCRIPT_HIGHCHARTS_PLAYER_LIFETIME_DIV_TAG.replace("PLAYERNAME", ezstatslib.escapePlayerName(pl.name))
         playersLifetimeDivStrs += "<br>\n"
     
