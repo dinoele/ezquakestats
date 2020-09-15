3039c33a838aaa1ceec478806ddb7ff1a2494394
dinoele <evlan@yandex.ru>
2020-09-15 14:57:18 +0300

    ezstatslib.py: version is incremented, new value is 1.27

--- ezstatslib.py
+++ ezstatslib.py
@@ -37,7 +37,7 @@ possibleColors = [HtmlColor.COLOR_RED,
                   HtmlColor.COLOR_CYAN,
                   HtmlColor.COLOR_MAGENTA]
 
-CURRENT_VERSION = "1.26"
+CURRENT_VERSION = "1.27"
                   
 LOG_TIMESTAMP_DELIMITER = " <-> "
 
